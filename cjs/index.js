f76a59446bca464b7f41b8540e0cbbc1b78d0ebd
sirkostya009 <kosta.tovstik@gmail.com>
2025-05-10 22:12:39 +0300

    generate cjs

--- cjs/index.js
+++ cjs/index.js
@@ -19,7 +19,7 @@ async function attestation(/** @type {AttestationResponse} */ a, /** @type {Atte
 		);
 	}
 
-	verifyChallenge(response.clientData.challenge, challenge);
+	verifyChallenge(response.clientData.challenge, opts.challenge);
 
 	switch (response.attestationObject.fmt) {
 		case "packed": // TODO: https://www.w3.org/TR/webauthn-2/#sctn-packed-attestation
@@ -60,9 +60,9 @@ async function assertion(/** @type {AssertionResponse} */ a, /** @type {Assertio
 		);
 	}
 
-	verifyChallenge(response.clientData.challenge, challenge);
+	verifyChallenge(response.clientData.challenge, opts.challenge);
 
-	let key = publicKey;
+	let key = opts.publicKey;
 
 	if ("1" in key) {
 		key = coseToJwk(key);
@@ -158,7 +158,7 @@ function verifyOrigins(/** @type {string} */ origin, /** @type {string[]} */ ori
 	}
 
 	if (!origins.includes(origin)) {
-		throw new Error(`Origin ${quoteString(clientData.origin)} not allowed`);
+		throw new Error(`Origin ${quoteString(origin)} not allowed`);
 	}
 }
 
